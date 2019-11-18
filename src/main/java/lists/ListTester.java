676ab147f75840daaa2d4229c8d88a944652077e
Julian Honer <julianhorner@web.de>
2019-11-18 12:14:55 +0100

    Add ListTester class

---
+++ src/main/java/lists/ListTester.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 (C) by Julian Horner.
+ * All Rights Reserved.
+ */
+
+package lists;
+
+/**
+ * Class for testing my list implementations.
+ * 
+ * @author julian
+ */
+public final class ListTester {
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * Tests the lists.
+     * 
+     * @param args The arguments
+     */
+    public static void main(final String[] args) {
+        List<Integer> list = new LinkedList<>();
+
+        // test add, get, size, isEmpty, toString, for-each and iterator
+        
+//        for (int i = 0; i < list.size(); i++) {
+//            System.out.println(list.get(i));
+//        }
+//        //Iteration using the iterator directly
+//        Iterator<Integer> it = list.iterator();
+//        while (it.hasNext()) {
+//            System.out.println(it.next());
+//        }
+//        //Iteration using for-each
+//        for (Integer i : list) {
+//            System.out.println(i);
+//        }
+    }
+
+    //----------------------------------------------------------------------------------------------
+
+    /**
+     * Hide constructor of utility class.
+     */
+    private ListTester() { }
+    
+    //----------------------------------------------------------------------------------------------
+}
