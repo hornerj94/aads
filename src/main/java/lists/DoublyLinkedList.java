3aa0e83ed0ec78c00ca4618c40dd5958a38b75c5
Julian Honer <julianhorner@web.de>
2019-11-18 18:14:22 +0100

    Add basic implementation of DoublyLinkedList

---
+++ src/main/java/lists/DoublyLinkedList.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2019 (C) by Julian Horner.
+ * All Rights Reserved.
+ */
+
+package lists;
+
+/**
+ * My implementation of a singly linked list.
+ * 
+ * @author julian
+ *
+ * @param <T> The type of the data of the list
+ */
+public abstract class DoublyLinkedList<T> implements List<T> { }
